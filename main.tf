6257c09ce28aaf91cd168344ee43b9bb576ba7e2
balatuluva <bala.tulva@gmail.com>
2023-12-07 06:28:54 +0000

    passwords variable for better security

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_ssm_parameter" "parameters" {
   type = "String"
   key_id = "b4ef00c9-7aa3-4bba-ad5d-54544604bbaa"
 }
+
+resource "aws_ssm_parameter" "passwords" {
+  count = length(var.passwords)
+  name = var.passwords[count.index].name
+  value = var.passwords[count.index].value
+  type = "SecureString"
+  key_id = "b4ef00c9-7aa3-4bba-ad5d-54544604bbaa"
+}
